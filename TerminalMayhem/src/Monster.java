771d075c38c531fdc3d8643d02e9287e7d0c2e32
David <david.gevriye@hotmail.com>
2023-03-21 23:00:06 +0100

    Börjar nu med subklassen för Monster

--- TerminalMayhem/src/Monster.java
+++ TerminalMayhem/src/Monster.java
@@ -65,6 +65,4 @@ public abstract class Monster {
 
 
 
-
-
 }
---
+++ TerminalMayhem/src/UnikMonster.java
@@ -0,0 +1,24 @@
+public class UnikMonster extends Monster {
+
+
+    public UnikMonster(String name, int HP, int strength, int toughness, int gold, int exp) {
+        super(name, HP, strength, toughness, gold, exp);
+    }
+
+    @Override
+    public int attack() {
+        return 0;
+    }
+
+    @Override
+    public int defence() {
+        return 0;
+    }
+
+
+
+
+
+}
+
+
