a1c510228fd3f920224a1dd18b9bae757e78246b
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2023-03-21 11:35:59 +0100

    - Definierat egen Pseudo

---
+++ TerminalMayhem/src/TextClass.java
@@ -0,0 +1,39 @@
+/* Class TextClass
+- Innehåller text; tex huvudmeny.
+- Alla är void
+
+Metod getWelcomeText()
+    Välkomsttext + be användaren att skriva in sitt namn.
+
+Metod getMainMenutext()
+    1.Starta spel
+    2.Skapa karaktär, ge namn och egenskaper.
+    3.Avsluta spel
+
+Metod getGameRoundMenu()
+
+Metod getShopMenu()
+    Välkommen till ShopMenu
+    behöver stå "vad väljer du ?" i slutet
+
+Metod getPlayerStatText()
+    Här är statsen för din hjälte
+
+Metod thanksForPlaying()
+    Sluttext Thank you for playing! Awesome game and performance by you good sire!
+
+Metod getBossFightText()
+    beskriver slutbossen för att ge mer inlevelse i spelet
+*/
+
+public class TextClass {
+
+
+
+
+
+
+
+
+
+}
