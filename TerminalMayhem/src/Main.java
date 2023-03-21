b6cf0c7039d319297ee778397e5ad66bde3a07ac
83waksug <119501535+83wakasug@users.noreply.github.com>
2023-03-21 16:56:49 +0100

    update

--- TerminalMayhem/src/Main.java
+++ TerminalMayhem/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+
+
+                //Game game = new Game();
+               Item item=new Item();
+               Player player=new Player(item,"wakana ");
+                //game.startGame();
+
+               player.showHero();
+
+
     }
 }
