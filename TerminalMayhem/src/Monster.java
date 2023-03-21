92f28b85bcfcfc3d9a3641d11ed086fc65b99b38
David <david.gevriye@hotmail.com>
2023-03-21 12:43:38 +0100

    en abstrakt klass lagt till getters hittills, ny update(12:42 2023-03-21): lagt till getters och setters till alla properties.

--- TerminalMayhem/src/Monster.java
+++ TerminalMayhem/src/Monster.java
@@ -1,25 +1,69 @@
 public abstract class Monster {
 
-    String name;
-    int HP;
-    int Strength;
-    int toughness;
-    int gold;
-    int exp;
+        private String name;
+        private int HP;
+        private int strength;
+        private int toughness;
+        private int gold;
+        private int exp;
 
+        public Monster(String name, int HP, int strength, int toughness, int gold, int exp) {
+            this.name = name;
+            this.HP = HP;
+            this.strength = strength;
+            this.toughness = toughness;
+            this.gold = gold;
+            this.exp = exp;
+        }
 
+        // Getters and setters for the properties
+        public String getName() {
+            return name;
+        }
 
-    public String getName() {return name;}
+        public void setName(String name) {
+            this.name = name;
+        }
 
-    public int getHP() {return HP;}
+        public int getHP() {
+            return HP;
+        }
 
-    public int getStrength() {return Strength;}
+        public void setHP(int HP) {
+            this.HP = HP;
+        }
 
-    public int getToughness() {return toughness;}
+        public int getStrength() {
+            return strength;
+        }
 
-    public int getGold() {return gold;}
+        public void setStrength(int strength) {
+            this.strength = strength;
+        }
 
-    public int getExp() {return exp;}
+        public int getToughness() {
+            return toughness;
+        }
+
+        public void setToughness(int toughness) {
+            this.toughness = toughness;
+        }
+
+        public int getGold() {
+            return gold;
+        }
+
+        public void setGold(int gold) {
+            this.gold = gold;
+        }
+
+        public int getExp() {
+            return exp;
+        }
+
+        public void setExp(int exp) {
+            this.exp = exp;
+        }
 
 
     public boolean checkIfDead(){
