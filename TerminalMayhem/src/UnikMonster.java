283d51b0765cbf5afcda578434c712cf8fcfd1db
David <david.gevriye@hotmail.com>
2023-03-21 23:12:39 +0100

    Har nu börjat sätta in värdena för de unika monstren, behöver först kolla med gruppen innan detta färdigställs, måste jämföra med playern. Om hur exp, guld ska tilldelas.

--- TerminalMayhem/src/UnikMonster.java
+++ TerminalMayhem/src/UnikMonster.java
@@ -97,6 +97,92 @@ public class UnikMonster extends Monster {
         }
 
     }
+    public class God extends Monster {
+        public God(String name, int HP, int strength, int toughness, int gold, int exp) {
+            super(name, HP, strength, toughness, gold, exp);
+        }
+
+        @Override
+        public int attack() {
+            return 0;
+        }
+
+        @Override
+        public int defence() {
+            return 0;
+        }
+
+    }
+
+    public class Death extends Monster {
+        public Death(String name, int HP, int strength, int toughness, int gold, int exp) {
+            super(name, HP, strength, toughness, gold, exp);
+        }
+
+        @Override
+        public int attack() {
+            return 0;
+        }
+
+        @Override
+        public int defence() {
+            return 0;
+        }
+
+    }
+    public class Knight extends Monster {
+        public Knight(String name, int HP, int strength, int toughness, int gold, int exp) {
+            super(name, HP, strength, toughness, gold, exp);
+        }
+
+        @Override
+        public int attack() {
+            return 0;
+        }
+
+        @Override
+        public int defence() {
+            return 0;
+        }
+
+    }
+    public class Mercenary extends Monster {
+        public Mercenary(String name, int HP, int strength, int toughness, int gold, int exp) {
+            super(name, HP, strength, toughness, gold, exp);
+        }
+
+        @Override
+        public int attack() {
+            return 0;
+        }
+
+        @Override
+        public int defence() {
+            return 0;
+        }
+
+    }
+
+    public class Slime extends Monster {
+        public Slime(String name, int HP, int strength, int toughness, int gold, int exp) {
+            super(name, HP, strength, toughness, gold, exp);
+        }
+
+        @Override
+        public int attack() {
+            return 0;
+        }
+
+        @Override
+        public int defence() {
+            return 0;
+        }
+
+    }
+    Zombie zombie = new Zombie("Zombie", 100, 10, 5, 50, 100);
+    Skeleton skeleton = new Skeleton("Skeleton", 80, 8, 3, 30, 80);
+    Dragon dragon = new Dragon("Dragon", 500, 50, 20, 500, 1000);
+    Demon demon = new Demon("Demon", 300, 60, 10, 300, 200);
 
 
 }
