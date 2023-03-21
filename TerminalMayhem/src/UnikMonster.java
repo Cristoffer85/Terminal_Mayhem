4ee0fcd4a05aefed046e3f443502807f24695836
David <david.gevriye@hotmail.com>
2023-03-21 23:01:08 +0100

    Dragon subklass

--- TerminalMayhem/src/UnikMonster.java
+++ TerminalMayhem/src/UnikMonster.java
@@ -16,6 +16,54 @@ public class UnikMonster extends Monster {
     }
 
 
+    public class Zombie extends Monster {
+        public Zombie(String name, int HP, int strength, int toughness, int gold, int exp) {
+            super(name, HP, strength, toughness, gold, exp);
+        }
+
+        @Override
+        public int attack() {
+            return 0;
+        }
+
+        @Override
+        public int defence() {
+            return 0;
+        }
+
+    }
+    public class Skeleton extends Monster {
+        public Skeleton(String name, int HP, int strength, int toughness, int gold, int exp) {
+            super(name, HP, strength, toughness, gold, exp);
+        }
+
+        @Override
+        public int attack() {
+            return 0;
+        }
+
+        @Override
+        public int defence() {
+            return 0;
+        }
+
+    }
+    public class Dragon extends Monster {
+        public Dragon(String name, int HP, int strength, int toughness, int gold, int exp) {
+            super(name, HP, strength, toughness, gold, exp);
+        }
+
+        @Override
+        public int attack() {
+            return 0;
+        }
+
+        @Override
+        public int defence() {
+            return 0;
+        }
+
+    }
 
 
 
