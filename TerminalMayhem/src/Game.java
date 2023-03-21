6da2a268d6cf8092bee3241a5f84d58a0c06df95
Emilsivertsson <Emil.sivertsson@gmail.com>
2023-03-21 12:13:27 +0100

    Started Go adventuring

--- TerminalMayhem/src/Game.java
+++ TerminalMayhem/src/Game.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 import java.util.function.ToDoubleBiFunction;
 
@@ -14,7 +15,9 @@ public class Game {
 
     boolean game = true;
 
+
     Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
 
     void startGame(){
         text.getWelcomeText();
@@ -22,10 +25,11 @@ public class Game {
 
         while(game){
             int mainMenuChoice = scanner.nextInt();
+            text.getMainMenu();
 
             switch (mainMenuChoice){
                 case 1:
-
+                    goAdventuring();
                     break;
                 case 2:
                     text.getPlayerStatText();
@@ -43,10 +47,39 @@ public class Game {
 
     }
 
+    private void goAdventuring() {
+        int isItAFight = random.nextInt(100);
+        if (isItAFight >=10){
+            fight();
+
+        } else {
+            text.nothingHappend();
+        }
+    }
+
+    private void fight() {
+        boolean isAnyOneDeadYet = true;
+        while (isAnyOneDeadYet){
+            attack(player.getStrength(), monstertyp.getDefence());
+            if (monstertyp.checkIfdead() == true){
+                isAnyOneDeadYet == false;
+            }
+            attack(monstertyp.getStrength(), player.getdefence());
+            if (player.checkIfdead() == true){
+                isAnyOneDeadYet == false;
+            }
+        }
+    }
+
+    private void attack() {
+        //random player strenght - 2* strenght mot monstrets
+    }
+
     private void goShopping() {
         text.getShopMenu();
         shop.showItems();
         int shopChoice = scanner.nextInt();
+        shop.buyItem(shopChoice);
         //TODO behöver bestämma om varan ska föras över, så fall behövs en metod för att skriva över från shop till player.
         // annars behöver metoden kolla om varan är en medaljon eller potion och sen agera uteifrån det på playerns stats.
 
