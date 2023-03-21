287bb91f47ea10ed2dc9b9f05dc0cc40299b709b
David <david.gevriye@hotmail.com>
2023-03-21 23:01:32 +0100

    Demon subklass

--- TerminalMayhem/src/UnikMonster.java
+++ TerminalMayhem/src/UnikMonster.java
@@ -65,6 +65,23 @@ public class UnikMonster extends Monster {
 
     }
 
+    public class Demon extends Monster {
+        public Demon(String name, int HP, int strength, int toughness, int gold, int exp) {
+            super(name, HP, strength, toughness, gold, exp);
+        }
+
+        @Override
+        public int attack() {
+            return 0;
+        }
+
+        @Override
+        public int defence() {
+            return 0;
+        }
+
+    }
+
 
 
 }
