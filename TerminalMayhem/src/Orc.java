606560d7aae846239c8bec8f046cfbe7dd512bd3
Emilsivertsson <Emil.sivertsson@gmail.com>
2023-03-23 14:19:36 +0100

    Sorted all monsters

---
+++ TerminalMayhem/src/Orc.java
@@ -0,0 +1,6 @@
+public class Orc extends Monster {
+    public Orc(String name, int lvl, int HP, int strength, int toughness, int gold, int exp) {
+        super(name, lvl, HP, strength, toughness, gold, exp);
+    }
+
+}
---
+++ TerminalMayhem/src/Skeleton.java
@@ -0,0 +1,9 @@
+public class Skeleton extends Monster {
+    public Skeleton(String name, int lvl, int HP, int strength, int toughness, int gold, int exp) {
+        super(name, lvl, HP, strength, toughness, gold, exp);
+    }
+
+}
+
+
+
