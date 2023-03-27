b7c42486d4c8caa8163c690b2e22bfe68542f015
Kristian Karlson <107461637+Bremmster@users.noreply.github.com>
2023-03-27 15:32:21 +0200

    refactor Game.goAdventuring

--- TerminalMayhem/src/Game.java
+++ TerminalMayhem/src/Game.java
@@ -32,7 +32,7 @@ public class Game {
             int mainMenuChoice = userInputInt();
 
             switch (mainMenuChoice) {
-                case 1 -> goAdventuring(player);
+                case 1 -> game = goAdventuring(player, game);
                 case 2 -> {
                     Text.getPlayerStatText();
                     player.showHero();
@@ -49,38 +49,46 @@ public class Game {
     }
 
     //method to go look for monsters
-    private void goAdventuring(Player player) {
-        int isItAFight = random.nextInt(100);
-        if (isItAFight >= 10) { // 90% chance of a fight
+    private boolean goAdventuring(Player player, boolean game) {
 
-            if (player.checkIfReadyForFinalBoss()) {    // check if player is ready for final boss
-                for (Monster monster : monsters) {
-                    if (monster.getLvl() == 10) {  // check if monster is final boss
-                        Text.getBossFightText();
-                        combat(player, monster);
-                        Text.getBossFightOverText();
-                        Text.pressToContinue();
-                        resetToDefault();
-                        break;
-                    }
-                }
+        int isItAFight = random.nextInt(100);
+        
+        if (isItAFight <= 10) { // 90% chance of a fight
+
+            Text.nothingHappened(); // 10% chance of nothing happening, prints message about it
+            Text.pressToContinue(); // waits for user input to continue
+
+        } else {
+
+            if (player.readyForFinalBoss()) {    // check if player is ready for final boss
+                game = finalEncounter(player, game); // last fight in the game
 
             } else {
                 for (Monster monster : monsters) {
                     if (monster.getLvl() == player.getLevel()) {
-                        Text.aMonsterAppears(monster, monster.getName());
-                        combat(player, monster);
+                        combat(player, monster, game);
                         monsters.remove(monster); // remove monster from list once defeated
                         break;
                     }
                 }
-                player.checkIfLevelUp();  // check if player has reached a new level and adds stats
-
             }
-        } else {
-            Text.nothingHappened(); // 10% chance of nothing happening, prints message about it
-            Text.pressToContinue(); // waits for user input to continue
         }
+        player.checkIfLevelUp();  // check if player has reached a new level and adds stats
+        return game;
+    }
+
+    private boolean finalEncounter(Player player, boolean game) {
+        for (Monster monster : monsters) {
+            if (monster.getLvl() == 10) {  // check if monster is final boss
+                Text.getBossFightText();
+                combat(player, monster, game);
+                Text.getBossFightOverText();
+                Text.pressToContinue();
+                return  !game; // breaks the mainSwitch while loop
+            }
+        }
+        System.out.println("Game.finalEncounter() exited with condition true"); // for debugging purpose only
+        return game; // not possible to en up here
     }
 
     //Give player XP, gold and Hp boost
@@ -94,7 +102,9 @@ public class Game {
     }
 
     //Initiate battle between player and selected monster.
-    private void combat(Player player, Monster monster) {
+    private boolean combat(Player player, Monster monster, boolean game) {
+
+        Text.aMonsterAppears(monster, monster.getName());
 
         while (true) {
             //User choice to attack or use a potion
@@ -124,10 +134,11 @@ public class Game {
             // If the player dies, breaks loop
             if (player.checkIfDead()) {
                 Text.getPlayerDead();
-                resetToDefault();
-                break;
+                return !game; // player died break mainSwitch
+
             }
         }
+        return game;
     }
 
     public void resetToDefault(){
