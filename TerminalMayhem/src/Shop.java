f2f4302ebc112c7fea9b54439596c4d474419049
Kristian Karlson <107461637+Bremmster@users.noreply.github.com>
2023-03-28 11:17:47 +0200

    comment's on Shop

--- TerminalMayhem/src/Shop.java
+++ TerminalMayhem/src/Shop.java
@@ -2,8 +2,10 @@ import java.util.ArrayList;
 
 /**
  * @author Kristian Karlson
- * Used by game for the goShopping
+ * Used by Game for the goShopping()
  * Created at start of Game. Shop fills in self with items
+ * Displays items
+ * Sells items as Object
  *
  */
 public class Shop {
@@ -14,7 +16,7 @@ public class Shop {
         /* Item properties = itemName +maxHP +strength + toughness +criticalChance + healingPotionValue Price
            Ok to create  +/-/0 values. But player stats will display bugged if player stats go below zero. */
         this.items.add(new Equipment("The Cat's Eye Amulet", 5, 5, 2, 1, 0, 69));
-        this.items.add(new Equipment("The Ring of the Unlucky", 0, 0, 0, -10, 0, 10));  // idé? ge varan negativt pris och ge spelaren möjlighet att inte equipa den
+        this.items.add(new Equipment("The Ring of the Unlucky", 0, 0, 0, -5, 0, 10));
         this.items.add(new Equipment("The Ruby axe of Joy", 0, 8, 0, 4, 0, 200));
         this.items.add(new Equipment("The Helmet of Uni-Brow", 0, 0, 6, 0, 0, 100));
         this.items.add(new HealingPotion());
@@ -30,13 +32,15 @@ public class Shop {
         /* display all available items in the shop.
            Used for each loop to make the code more readable
            index to select items from when buying. Game reduces user input with -1 to point at the correct item
-
+           Heal potions printed separately for possibility to give different look in shop
+           All other items printed with @Override toString. @ time of writing this comment other type of Item was Equipment
         */
-        int index = 1;  //
-        for (Item item : items) {  //
-            if (item instanceof HealingPotion) {   // different println for Healing potions
+        int index = 1;
+        for (Item item : items) {
+
+            if (item instanceof HealingPotion) {
                 System.out.println(index + " " + item.getName() + ", adds " + item.getPotionValue() + " Health points, price: " + item.getPrice() + " gold");
-            } else { // print all other items with @Override toString. when this comment were written only other typ was Equipment
+            } else {
                 System.out.println(index + " " + item.toString()); // prints index number for user and item stats that are !0
             }
             index++;
@@ -44,16 +48,22 @@ public class Shop {
     }
 
     public int getPrice(int itemNumber) {
-        return items.get(itemNumber).getPrice();  // returns the price the class Game checks if the user can afford the item
+        // Used by game to check if player can afford the item
+        return items.get(itemNumber).getPrice();
     }
 
-    public Item buyItem(int itemNumber) {   // if the user affords the item
+    public Item buyItem(int itemNumber) {
+        /* if the game.goShopping() check for enough gold passes
+           Send the item to goShopping. goShopping gives item to player.
+           Bought items are removed from shop
+        */
         Item copyObject = items.get(itemNumber);
-        items.remove(itemNumber); // removes the item from the shop
+        items.remove(itemNumber);
         return copyObject;
     }
 
     public String getName(int itemNumber) {
+        // used by game-goShopping to display that the item its bought
         return items.get(itemNumber).getName();
     }
 }
